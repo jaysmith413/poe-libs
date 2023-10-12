1d5cdb1c241982b61121e77786f2ad1215f0eb91
jaysmith413 <jaysmith413@gmail.com>
2023-10-12 14:02:21 -0500

    MVP

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>&copy; jaysmith413, {{new Date().getFullYear()}}</h1>
+    </header>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+header {
+    background-color: lightgray;
+
+}
+</style>
